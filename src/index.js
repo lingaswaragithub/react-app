26f309cc531ce53fb50e5fa8bd7a2a1c4b5131b8
lingaswaragithub <lingaswara.mallidi@gmail.com>
2021-08-27 21:23:19 -0400

    updated message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element= <h1>hellow world</h1>;
+const element= <h1>hellow world, happy week end</h1>;
 console.log(element);
 
 ReactDOM.render(element,document.getElementById('root'));
